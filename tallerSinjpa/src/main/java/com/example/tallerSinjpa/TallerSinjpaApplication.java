1f1c5bf943cef2191592f7a9836559d74f307399
rowin <rowin224@gmail.com>
2025-10-10 08:54:49 -0500

    taller sinjpa

---
+++ tallerSinjpa/src/main/java/com/example/tallerSinjpa/TallerSinjpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.tallerSinjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerSinjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerSinjpaApplication.class, args);
+	}
+
+}
